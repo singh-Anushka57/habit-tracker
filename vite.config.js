2f450a5760ff5f4ba22a1d509fa9e80f77b2e059
singh-Anushka57 <singh.anushka1062@gmail.com>
2026-06-23 10:56:58 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/habit-tracker/',   // ← change this to your repo name
-})git add vite.config.js
-git commit -m "fix base path for GitHub Pages"
-git push
+  base: '/habit-tracker/'
+})
